405d863033db89bc33375b7f1e7e930298550cca
J. TURNER <turner.j6@gmail.com>
2015-03-26 17:26:08 -0700

    Amend users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,10 @@
 class UsersController < ActionController::Base
+  before_action :authenticate_user, except: [:index]
 
   def index
   end
 
-  def new
-    @user = User.new
-    @users = User.all
-    render :new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "Your user name has been established."
-      redirect_to user_path(@user)
-    else
-      flash[:alert] = "Please stablish a user name."
-      @users = User.all
-      render :new
-    end
+  def show
   end
 
 end
